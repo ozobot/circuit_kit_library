035d37c2832c82e4de125f296412e3ae74ce1235
ondrap <pilat.ondrej@seznam.cz>
2026-05-22 14:57:04 +0200

    feat: Add blink example. It uses chassis RGB LED.

---
+++ examples/blink/blink.ino
@@ -0,0 +1,33 @@
+/// This example needs Freenove ESP32 WS2812 library to be installed
+
+#include "drvkit.h"
+#include "Freenove_WS2812_Lib_for_ESP32.h"
+
+using namespace ozobot::drvkit;
+
+#define LEDS_COUNT  1 /// We have only single LED on the board
+#define LEDS_PIN	RGB_LED
+#define CHANNEL		0
+
+Freenove_ESP32_WS2812 strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL);
+
+void setup() {
+  Serial.begin(9600);
+  strip.begin();
+
+  pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+  Serial.printf("Test\r\n");
+
+  /// Setup pixel 0 (we have only single LED) next color to low green (RGB)
+  strip.setLedColorData(0, 0 , 30, 0);
+  strip.show(); /// Write prepared colors to strip 
+  delay(200);
+
+  /// Setup pixel 0 (we have only single LED) next color to black (RGB)
+  strip.setLedColorData(0, 0 , 0, 0);
+  strip.show(); /// Write prepared colors to strip
+  delay(200);
+}
